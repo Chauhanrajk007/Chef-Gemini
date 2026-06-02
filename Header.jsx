3896c0d809c5718d63c88652b0f4bce09ba88d29
Tinkudev <188956193+Chauhanrajk007@users.noreply.github.com>
2026-06-02 13:08:40 +0530

    Update Header.jsx

--- Header.jsx
+++ Header.jsx
@@ -2,7 +2,7 @@ import ChefIcon from "./images/chef-gemini-icon.jpg"
 export default function Header(){
   return (
     <header>
-      <img src={ChefIcon} alt=icon />
+      <img src={ChefIcon} alt="icon" />
       <h1>Chef Gemini</h1>
     </header>
 )
