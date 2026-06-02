9bcaafd7e53678cad8047fd1db1c95f59932e5e4
Tinkudev <188956193+Chauhanrajk007@users.noreply.github.com>
2026-06-02 15:51:26 +0530

    Update Main.jsx

--- Main.jsx
+++ Main.jsx
@@ -1,8 +1,17 @@
 export default function Main(){
+  const ingredients=[]
+  const ingredientsListItems = ingredients.map(ingrdients=>(<li key={ingredient}>{ingredient}</li>li>))
+  function handleSubmit(event)
+  {
+    event.preventDefault()
+    const formData=new FormData(event.currentTarget)
+    const newIngredient=formData.get("ingredient")
+    ingredients.push(newIngredient)
+  }
 return (
   <main>
-    <form>
-      <input  type="text" placeholder="e.g. oregano" aria-label="Add ingredient" />
+    <form onSubmit={handleSubmit}>
+      <input  type="text" placeholder="e.g. oregano" aria-label="Add ingredient" name="ingredient"/>
       <button>Add ingredient</button>
     </form>
   </main>
