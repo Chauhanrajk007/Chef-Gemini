23e868f0591eca65494fbfb6d792673e13b7b575
Tinkudev <188956193+Chauhanrajk007@users.noreply.github.com>
2026-06-08 17:12:53 +0530

    Update Main.jsx

--- Main.jsx
+++ Main.jsx
@@ -2,16 +2,14 @@ import React from "react"
 export default function Main(){
   const [ingredients,setIngredients]=React.useState([])
   const ingredientsListItems = ingredients.map(ingredients=>(<li key={ingredients}>{ingredients}</li>))
-  function handleSubmit(event)
+  function addIngredient(formData)
   {
-    event.preventDefault()
-    const formData=new FormData(event.currentTarget)
     const newIngredient=formData.get("ingredient")
     setIngredients(ingredients=>[...ingredients,newIngredient])
   }
 return (
   <main>
-    <form onSubmit={handleSubmit}>
+    <form action={addIngredient}>
       <input  type="text" placeholder="e.g. oregano" aria-label="Add ingredient" name="ingredient"/>
       <button>Add ingredient</button>
     </form>
